f0657f7466cffa372821d92dced005dd5f0b07d7
Crowdbotics <team@crowdbotics.com>
2024-03-27 09:38:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "double-data-offer-47298",
+  url: "https://double-data-offer-47298.botics.co",
+  api: "https://double-data-offer-47298.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
